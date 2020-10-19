32ead53bc608567094ee6cd6a8bc0487dfb36e69
SL <sangwe13@gmail.com>
2020-10-19 06:49:17 -0400

    updating cylinder

--- Test.py
+++ Test.py
@@ -3,17 +3,19 @@ import sys
 import maya.cmds as cmds
 import numpy as np
 import scipy as sp
+from scipy.spatial import Voronoi, voronoi_plot_2d
+from scipy.spatial import Delaunay
+
 import math
 import vector3d
 
 from anytree import Node, RenderTree, NodeMixin
 vector = vector3d.vector.Vector(0,1,0)
 
-
-N = 700
-x = -10 + 10* np.random.rand(N)
-y = 1 +   10* np.random.rand(N)
-z = -10 + 10*np.random.rand(N)
+N = 50
+x = -5 + 5* np.random.rand(N)
+y = 1 +  4* np.random.rand(N)
+z = -5 + 5*np.random.rand(N)
 
 #make a list of spheres/points
 cmds.polySphere(r=0.07)
@@ -31,10 +33,9 @@ for i in range(N):
     instanceResult = cmds.instance(transformName, name=transformName+'_instance#')
     cmds.parent(instanceResult, instanceGroupName)
     cmds.move(x[i], y[i], z[i], instanceResult)
-    list_pts.append(Point([x[i], y[i], z[i]]))
+    list_pts.append(Point([ x[i], y[i], z[i] ]))
    
 cmds.hide(transformName)
-boundPts = cmds.exactWorldBoundingBox(transformName+'_instance_grp1')
 
 #with the random points, start from a beginning tree node
 
@@ -42,22 +43,23 @@ class MyBaseClass(object):
     foo = 4
 
 class TreeNode(MyBaseClass, NodeMixin):
-    def __init__(self, name, pos, pts=None, parent=None, children=None):
+    def __init__(self, name, pos, rad=None, pts=None, parent=None, children=None):
          super(TreeNode, self).__init__()
          self.name = name
+         self.rad = rad
          self.pos = pos
          self.pts = pts
          self.parent = parent
          if children:
              self.children = children
              
-    def addChild(self, pos):
+    def addChild(self, node):
         print("added child")
-        if isinstance(pos, list):
-            new_node =(TreeNode(self.name, pos, parent=self))               
+        if self.children:
+            np.append(self.children, node)             
           
     def addPts(self, pos):
-        print('added point')
+        #print('added point')
         if self.pts == None:
             self.pts = [pos]
         else: 
@@ -67,29 +69,30 @@ class TreeNode(MyBaseClass, NodeMixin):
 #for pre, fill, node in RenderTree(root):
 #    print("%s%s" % (pre, node.name)) 
 #tree formation     
-    
-list_node =[]
-
+boundPts = cmds.exactWorldBoundingBox(transformName+'_instance_grp1')
 midx = (boundPts[3] + boundPts[0])/2
 midz = (boundPts[5] + boundPts[2])/2
-
-root = TreeNode('root', [midx, 0, midz])
-list_node.append(root)
 
 cmds.polySphere(r=0.07)
 result = cmds.ls(orderedSelection = True)
 transformName = result[0]
 instanceGroupName = cmds.group(empty=True, name=transformName+'_instance_grp#')
 
-init_node = 6
+root = TreeNode('root', [midx, 0, midz])
+
+list_node =[root]
+
+init_node_num = 9
 init = root
-#create root
-for i in range(1, init_node):
+
+for i in range(1, init_node_num):
     instanceResult = cmds.polySphere(transformName,r=0.1, name=transformName+'_instance#')    
     cmds.parent(instanceResult, instanceGroupName)
-    cmds.move(midx, i*0.3, midz, instanceResult) 
- 
-    node = TreeNode('rootnode:'+str(i),[midx, i*0.3, midz],parent=init)
+    
+    pos = [midx, i*0.3, midz]
+    cmds.move(pos[0], pos[1], pos[2], instanceResult) 
+   
+    node = TreeNode('rootnode:'+str(i), pos, parent=init)
     list_node.append(init)
     init = node
 
@@ -98,26 +101,28 @@ for pre, fill, node in RenderTree(root):
     
 cmds.hide(transformName)
 
+
+def angle(v1, v2):
+    return np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
+        
 def distance (p1, p2):
     return math.sqrt( ((p1[0]-p2[0])**2)+((p1[1]-p2[1])**2)+((p1[2]-p2[2])**2) )
 
 def length (p1):
-    return math.sqrt( ((p1[0])**2)+((p1[1])**2)+((p1[2])**2) )
+    return ( ((p1[0])**2)+((p1[1])**2)+((p1[2])**2) )
     
+
 #try out space colonization:
 #radius of influence/kill distance
-i_d = 1.2
-k_d = 0.8
+i_d = 1.5
+k_d = 0.4
 
 #check through the pts and find the closest tree node
 cmds.select( clear=True )
 result = cmds.ls(orderedSelection = True)
 
-iter = 7
-print("iterations:",iter)
-
+iter = 2
 for i in range(iter):  
-  
     #find points close in influence distance
     for pt in list_pts:
         for node in list_node:
@@ -150,14 +155,19 @@ for i in range(iter):
                                   
             new_loc = [vec[0] + node.pos[0], vec[1] + node.pos[1], vec[2] + node.pos[2]]
             cmds.move(new_loc[0], new_loc[1], new_loc[2], instanceResult) 
-            
             print("final position:", new_loc)
+            list_tri_pos.append(new_loc)
 
             #add to new nodes list
-            node = TreeNode('node' + str(i), new_loc, parent=node)
-            list_newnodes.append(node)
+            new_node = TreeNode('node' + str(i), new_loc, parent=node)
+            print("this node has a parent:",new_node.parent.name)
+            list_newnodes.append(new_node)
+            
             #add to tree nodes
-            #node.addChild(new_loc)
+            #if node.children:
+                #print(node.children)
+            #else:
+                #node.children = np.array([new_node])
 
     #check for kill distance
     for node in list_newnodes:
@@ -171,36 +181,43 @@ for i in range(iter):
          
     list_node.extend(list_newnodes)
     
+    
+    
+    
 for pre, fill, node in RenderTree(root):
     print("%s%s" % (pre, node.name)) 
-        
 
+       
 def createBranches(node):
     if node.children:
         for child in node.children:
             print('node:', child.pos)
 
-cmds.polyCylinder(r=0.07, height=0.5)
+#print(list_tri_pos)
+#list_triangle = np.asarray(list_tri_pos)
+#tri = Delaunay(list_triangle)
+
+#print(tri.simplices)
+
+
+cmds.polyCylinder(r=0.07, height=0.02)
 result = cmds.ls(orderedSelection = True)
 transformName = result[0]
 instanceGroupName = cmds.group(empty=True, name=transformName+'_branchesGroup#')
 
-def angle(v1, v2, acute):
-# v1 is your firsr vector
-# v2 is your second vector
-    angle = np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
-    if (acute == True):
-        return angle
-    else:
-        return 2 * np.pi - angle
-            
+ 
 #createBranches(root)
 for pre, fill, node in RenderTree(root):
-    instanceResult = cmds.polyCylinder(transformName,r=0.1, name=transformName+'_branch#')
-    cmds.parent(instanceResult, instanceGroupName)                                  
-    cmds.move(node.pos[0], node.pos[1], node.pos[2], instanceResult) 
+    print("%s%s" % (pre, node.name)) 
 
-    print("%s%s%s" % (pre, node.pos, node.name)) 
+    if node.parent:
+        print("there is a parent")
+        axis = [node.pos[0]-node.parent.pos[0],node.pos[1]-node.parent.pos[1],node.pos[2]-node.parent.pos[2]] 
+        length = distance(node.parent.pos, node.pos)
+        cylinder2 = cmds.polyCylinder(transformName,r=0.02, axis=axis, height=length, name=transformName+'_branch#')
+        cmds.parent(cylinder2, instanceGroupName)                                  
+        cmds.move(node.pos[0], node.pos[1], node.pos[2], cylinder2)  
+        print("%s %s" % (node.parent.name, node.name)) 
 
      
     
